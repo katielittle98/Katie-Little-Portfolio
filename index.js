6170cc95497050844f6e1906edb8340770813d0b
Katie Little <katieanne.460@gmail.com>
2023-05-10 21:48:12 -0700

    added files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const project1Btn = document.getElementById('p1btn')
+const project2Btn = document.getElementById('p2btn')
+const project3Btn = document.getElementById('p3btn')
+
+
+project1Btn.addEventListener("click", function() {
+    location.href = 'project1.html'
+})
+
+project2Btn.addEventListener("click", function() {
+    location.href = 'project2.html'
+})
+
+project3Btn.addEventListener("click", function() {
+    location.href = 'project3.html'
+})
+
