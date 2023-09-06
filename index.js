9a2c36e485698b3069c47b1d8ada65c10fe246a3
Katie Little <katieanne.460@gmail.com>
2023-09-05 17:46:48 -0700

    updated UI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const project1Btn = document.getElementById('p1btn')
-const project2Btn = document.getElementById('p2btn')
-const project3Btn = document.getElementById('p3btn')
+const project1Btn = document.getElementById('p1btn');
+const project2Btn = document.getElementById('p2btn');
+const project3Btn = document.getElementById('p3btn');
+const project4Btn = document.getElementById('p4btn');
 
 
 project1Btn.addEventListener("click", function() {
@@ -13,5 +14,9 @@ project2Btn.addEventListener("click", function() {
 
 project3Btn.addEventListener("click", function() {
     location.href = 'project3.html'
+})
+
+project4Btn.addEventListener("click", function() {
+    location.href = 'project4.html'
 })
 
